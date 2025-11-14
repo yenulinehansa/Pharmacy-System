016ea4240b07e89191cef569fa7735ba6eee817e
yenulinehansa <yenulinehansa739@gmail.com>
2025-11-14 18:59:08 +0530

    Design frontpage ui

---
+++ src/main/java/Controller/FrontPageController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FrontPageController {
+
+    @FXML
+    private JFXRadioButton EmployeeRadio;
+
+    @FXML
+    private JFXRadioButton ManagerRadio;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private Label lblForgotPassword;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+}
